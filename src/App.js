196db8f8ea35cb4a21de676c0ce070f11d5b109e
Thomas Földi <thomas.foeldi@janitza.de>
2021-10-21 15:41:44 +0200

    added sign in page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./components/signin-singup/signin-signup.component";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route exact path='/shop' component={ShopPage}/>
+                <Route exact path='/signin' component={SignInAndSignUpPage}/>
             </Switch>
         </div>
     );
